b56d8ebea2038b9aff733114135137c2e7fa4a03
11kainani <95407428+11kainani@users.noreply.github.com>
2025-08-21 16:12:19 +0100

    Create main file to centralize the program

--- main.py
+++ main.py
@@ -1,36 +1,14 @@
-import boto3
-import pandas as pd
+import time
 
-from Extracter import Extracter
-from db.SQLConnector import SQLConnector
+from db.Transformation import Transformer
 
-class SQLController():
-    def __init__(self):
-        self.cloud_extracter = Extracter()
-        self.sql_connector = SQLConnector()
-
-    def create_course_table(self):
-        for course in self.cloud_extracter.courses_list:
-            self.sql_connector.create_course(course)
-
-    def create_week_table(self):
-        self.sql_connector.create_week(max_number_weeks=10)
-    def create_skill_table(self):
-        skills = self.cloud_extracter.skills_list
-        self.sql_connector.create_skills(skill_list=skills)
-
-    def create_trainer_table(self,name):
-        self.sql_connector.create_trainer(name)
 
 def main():
-    controller = SQLController()
-
-    controller.create_skill_table()
-    controller.create_week_table()
-    #create_course_table(ext, sql_server)
-    #
-
-
+    start = time.perf_counter()
+    transformer = Transformer()
+    transformer.academy_data_injection()
+    end = time.perf_counter()
+    print(f"Execution time: {end - start:.4f} seconds")
 
 
 if __name__ == "__main__":
